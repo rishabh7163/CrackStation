273986e4502d751071ba38b4312c584007085083
rishabh7163 <40897005+rishabh7163@users.noreply.github.com>
2022-11-25 13:37:20 -0800

    New Test Case for 3 Chars "??7"

--- Tests/CrackStationTest/CrackStationTest.swift
+++ Tests/CrackStationTest/CrackStationTest.swift
@@ -6,8 +6,8 @@ import XCTest
 final class CrackStationTest: XCTestCase{
     func testcrackstationtesting(){
         let l = CrackStation();
-        let final_test = l.decrypt(shaHash:"bbeebd879e1dff6918546dc0c179fdde505f2a21591c9a9c96e36b054ec5af83")
-        XCTAssertEqual(final_test,"Z")
+        let final_test = l.decrypt(shaHash:"745ce947be584a9eea85fbd23672b840dae9da74")
+        XCTAssertEqual(final_test,"??7")
 
     }
 }
