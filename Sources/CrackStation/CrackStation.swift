b00f1cb61df7be8d2dcbedc5afca4775dddf8e46
rishabh7163 <40897005+rishabh7163@users.noreply.github.com>
2022-12-04 15:06:44 -0800

    Update CrackStation.swift

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -1,24 +1,25 @@
 import Foundation
 
-public struct CrackStation: Decrypter {
-    init(){
+public protocol Decrypter {
+    init()
     func decrypt(shaHash: String) -> String?
 }
+public struct CrackStation: Decrypter {
 
-    var mvp_dict: Dictionary <String,String> = [:]
+    var lookupTable: Dictionary <String,String> = [:]
     public init() {
         do{
             guard let path = Bundle.module.url(forResource: "data", withExtension: "json") else {return}
             let data = try Data(contentsOf: path)
-            let jsonResult = try JSONSerialization.jsonObject(with: data) as? Dictionary<String, String> 
-            mvp_dict = jsonResult ?? [:]
+            let jsonoutput = try JSONSerialization.jsonObject(with: data) as? Dictionary<String, String>
+            lookupTable = jsonoutput ?? [:]
         } catch {
                 print("Error Loading from dictionary")
         }
     }
     
     public func decrypt(shaHash: String) -> String? {
-        let password = mvp_dict[shaHash]
+        let password = lookupTable[shaHash]
         return password
     }  
 }
